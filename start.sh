6b0c9d85b8c0b17db4d8d057e68052be2c35569a
Sicelo Mduna <siceloit@gmail.com>
2026-01-09 20:22:11 +0200

    update 1

--- start.sh
+++ start.sh
@@ -4,8 +4,14 @@ set -e
 echo "Starting n8n..."
 n8n start &
 
-echo "Waiting for broker to be ready..."
+# Give broker a few seconds to start
 sleep 5
+
+echo "Starting JavaScript task runner..."
+n8n task-runner js \
+  --broker-host 127.0.0.1 \
+  --broker-port 5679 \
+  --auth-token "$N8N_RUNNERS_AUTH_TOKEN" &
 
 echo "Starting Python task runner..."
 n8n task-runner python \
